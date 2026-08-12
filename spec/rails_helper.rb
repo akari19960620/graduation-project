66a1ddf087b0269de1bd368243dfe117b35ef50b
Your Name <you@example.com>
2026-08-12 16:09:59 +0900

    SimpleCovの導入

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,10 @@
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter '/bin/'
+  add_filter '/db/'
+  add_filter '/spec/'
+  add_filter '/test/'
+end
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
